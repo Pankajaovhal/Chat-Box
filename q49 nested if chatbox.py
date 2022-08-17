9f7dcb6bf9cd68fbfd7fc37311a6b6eca1f561a2
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2022-08-17 11:43:45 +0530

    Add files via upload

---
+++ q49 nested if chatbox.py
@@ -0,0 +1,45 @@
+chat=input("start chat:")
+if chat=="hie":
+    print("hello")
+    chat1=input("enter:")
+    if chat1=="what doing??":
+        print("nothing just having a littel coffee")
+        chat3=input("enter:")
+        if chat3=="what about you??":
+            print("just completed work")
+            chat4=input("enter:")
+            if chat4=="dinner??":
+                print("not yet")
+                chat5=input("enter:")
+                if chat5=="your's?":
+                    print("just going to have")
+                    chat6=input("enter:")
+                    if chat6=="how is the new job":
+                        print("not that bad")
+                        chat7=input("enter:")
+                        if chat7=="so what do you think about the delhi contract":
+                            print("i think it is good")
+                            chat8=input("enter:")
+                            if chat8=="okay than we will catchup letter":
+                                print("ya sure")
+                                chat9=input("enter:")
+                                if chat9=="okay than bye":
+                                    print("bye")
+                                else:
+                                    print("??")    
+                            else:
+                                print("??")
+                        else:
+                            print("??")
+                    else:
+                        print("??")
+                else:
+                    print("??")
+            else:
+                print("??")
+        else:
+            print("??")
+    else:
+        print("??")            
+else:
+    print("??")
